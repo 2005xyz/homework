458febeeafc42bc7574073ce63751796e0648640
2005xyz <146085924+2005xyz@users.noreply.github.com>
2025-10-10 12:45:47 +0800

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -4,24 +4,27 @@ using namespace std;
 int main() {
     vector<int> s;
     vector<int> if_s(20001, 1);
-    for (int i = 2; i < 20001; i++) {
+    if_s[0] = if_s[1] = 0;
+
+    for (int i = 3; i < 20001; i += 2) {
         if (if_s[i]) s.push_back(i);
         for (int j : s) {
-            if (1LL * j * i > 20000) break;
-            if_s[j * i] = 0;
+            int64_t x = 1LL * j * i;
+            if (x > 20000) break;
+            if_s[x] = 0;
             if (i % j == 0) break;
         }
     }
 
+    cout << 2 << ' ';
     int p = 0;
+    int cnt = 1;
     while (p < (int)s.size()) {
-        int cnt = 0;
-        while (cnt < 5 && p < (int)s.size()) {
-            cout << s[p] << ' ';
-            p++;
-            cnt++;
-        }
-        cout << endl;
+        cout << s[p] << ' ';
+        p++;
+        cnt++;
+        if (cnt % 5 == 0) cout << '\n';
     }
+    cout << endl;
     return 0;
 }
